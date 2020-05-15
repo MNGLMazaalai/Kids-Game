c30f53ec39ad88fc7c6609ff36a3b70234b7e8da
А. амаржаргал <n.nekogasuki643@gmail.com>
2020-05-15 18:24:14 +0800

    json data

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "79c7ec64bdf51ccae4db";
+/******/ 	var hotCurrentHash = "37bea9ac6dc4d838ac71";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

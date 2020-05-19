26b04064fb923a2945df7077e2866428855c12b7
А. амаржаргал <n.nekogasuki643@gmail.com>
2020-05-19 20:24:03 +0800

    deleted level.js

--- pages/levels/level.js
+++
@@ -1,9 +0,0 @@
-import path from 'path'
-// import fs from 'fs'
-// const postDirectory = path.join(process.cwd(), 'pages/levels')
-
-export default function Hello() {
-    // const levelname = fs.readdirSync(postDirectory)
-    // console.log(levelname)
-    return <div> {levelname}</div>
-}

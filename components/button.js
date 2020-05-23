3fbe647baeaf3b09f7ebf71393b1e93889c5b965
Бат-Эрдэнэ Б <n.baterdene.goodgodking@gmail.com>
2020-05-23 14:11:30 +0800

    tabsize fix

--- components/button.js
+++ components/button.js
@@ -7,64 +7,64 @@ import utilStyles from '../styles/utils.module.scss'
 const images = "/images/categoryPictures/"
 
 export function renderNewButton({props, name, color}) {
-    const currentSrc = images + props
-    return (
-        <section className={utilStyles.new} key={name}>
-            <section className={utilStyles.newBox}>
-                <section className={utilStyles.space}/>
-                <section className={utilStyles.newButton} style={{backgroundColor: color}}/>
-            </section>
-            <section className={utilStyles.newBox}>
-                <section className={utilStyles.imagebox}>
-                    <img
-                    className={utilStyles.newImage}
-                    src={currentSrc}
-                    />
-                </section>
-                <section className={utilStyles.text}>{name}</section>
-            </section>
+  const currentSrc = images + props
+  return (
+    <section className={utilStyles.new} key={name}>
+      <section className={utilStyles.newBox}>
+        <section className={utilStyles.space}/>
+        <section className={utilStyles.newButton} style={{backgroundColor: color}}/>
+      </section>
+      <section className={utilStyles.newBox}>
+        <section className={utilStyles.imagebox}>
+          <img
+          className={utilStyles.newImage}
+          src={currentSrc}
+          />
         </section>
-    );
+        <section className={utilStyles.text}>{name}</section>
+      </section>
+    </section>
+  );
 }
 export function renderButton({props, name, color}) {
-    const currentSrc = images + props
-    const id = props.replace(/\.png$/, '')
-    let count = 0;
-    return (
-        <Link href="./levels/level" as={`./levels/${id}`} key={currentSrc} passHref>
-            <section className={utilStyles.normal} key={name}>
-                <section className={utilStyles.box}>
-                    <section className={utilStyles.newSpace}/>
-                    <section className={utilStyles.button} style={{backgroundColor: color}}/>
-                </section>
-                <section className={utilStyles.box}>
-                    <section className={utilStyles.newImagebox}>
-                        <img
-                        className={utilStyles.image}
-                        src={currentSrc}
-                        />
-                    </section>
-                    <section className={utilStyles.text}>{name}</section>
-                </section>
-            </section>
-        </Link>
-    );
+  const currentSrc = images + props
+  const id = props.replace(/\.png$/, '')
+  let count = 0;
+  return (
+    <Link href="./levels/level" as={`./levels/${id}`} key={currentSrc} passHref>
+      <section className={utilStyles.normal} key={name}>
+        <section className={utilStyles.box}>
+          <section className={utilStyles.newSpace}/>
+          <section className={utilStyles.button} style={{backgroundColor: color}}/>
+        </section>
+        <section className={utilStyles.box}>
+          <section className={utilStyles.newImagebox}>
+            <img
+            className={utilStyles.image}
+            src={currentSrc}
+            />
+          </section>
+          <section className={utilStyles.text}>{name}</section>
+        </section>
+      </section>
+    </Link>
+  );
 }
 export function renderCategory() {
-    let res = [], count = 0
-    list.forEach(element => {
-        res.push (
-            <section className={utilStyles.line} key={count++}> 
-                {element.map(el => renderButton({props: el.photo, name: el.name, color: el.color}))}
-            </section>
-        )
-    })
-    return res;
+  let res = [], count = 0
+  list.forEach(element => {
+    res.push (
+      <section className={utilStyles.line} key={count++}> 
+        {element.map(el => renderButton({props: el.photo, name: el.name, color: el.color}))}
+      </section>
+    )
+  })
+  return res;
 }
 export function renderSlider() {
-    let res = []
-    newList.forEach(el => {
-        res.push(renderNewButton({props: el.photo, name: el.name, color: el.color}))
-    })
-    return res;
+  let res = []
+  newList.forEach(el => {
+    res.push(renderNewButton({props: el.photo, name: el.name, color: el.color}))
+  })
+  return res;
 }
--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,22 @@ import Head from 'next/head'
 import utilStyles from '../styles/utils.module.scss'
 import { renderSlider, renderCategory } from "../components/button";
 export default function Home() {
-
-    return (
+  return (
     <section className={utilStyles.homepage} >
-        <Head>
-            <title>{"Home page"}</title>
-        </Head>
-        <section className={`${utilStyles.text} ${utilStyles.title}`}>
-            {"Шинээр нэмэгдсэн"}
+      <Head>
+        <title>{"Home page"}</title>
+      </Head>
+      <section className={`${utilStyles.text} ${utilStyles.title}`}>
+        {"Шинээр нэмэгдсэн"}
+      </section>
+      <section className={utilStyles.slider}>
+        <section className={utilStyles.newBox}>
+          {renderSlider()}
         </section>
-        <section className={utilStyles.slider}>
-            <section className={utilStyles.newBox}>
-                {renderSlider()}
-            </section>
-        </section>
-
-        <section className={utilStyles.categories}>
-            {renderCategory()}
-        </section>
+      </section>
+      <section className={utilStyles.categories}>
+        {renderCategory()}
+      </section>
     </section>
-    )
-  }
+  )
+}

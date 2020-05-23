bb937f16f196ed3ba158ef75e3172352da659760
А. амаржаргал <n.nekogasuki643@gmail.com>
2020-05-23 14:05:46 +0800

    removing .next

--- pages/levels/[level].js
+++ pages/levels/[level].js
@@ -1,11 +1,13 @@
-// import fs from 'fs'
+import UIfx from 'uifx'
 import { useRouter } from "next/router";
 // const postsDirectory = path.join(process.cwd(), '')
 
 export default function Hello({ params }) {
   // const fileNames = fs.readdirSync(postsDirectory)
-  const router = useRouter();
-  console.log(router.query.level);
+  const router = useRouter()
+  const name = router.query.level
+  const mp3file = `../../public/audios/${name}/${name}1`
+  const sound = new UIfx(mp3file)
 
-  return <div> {params} </div>;
+  return <div> onclick={sound.play} {name} </div>;
 }
